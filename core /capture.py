46cef860c815b6f6a1f7257ac8329e32f1b64386
whoiz082 <muhammadzeeshan.csec@gmail.com>
2026-06-20 02:56:20 +0500

    Implement live packet capture functionality
    Added a function to start live packet capture using Scapy.

--- core /capture.py
+++ core /capture.py
@@ -1,1 +1,13 @@
+from scapy.all import sniff
 
+
+def start_capture(packet_callback):
+
+    print("=" * 60)
+    print(" Starting Live Packet Capture... Press Ctrl+C to stop ")
+    print("=" * 60)
+
+    sniff(
+        prn=packet_callback,
+        store=0
+    )
