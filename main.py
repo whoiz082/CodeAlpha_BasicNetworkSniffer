01521a8b26049b34f90e87bc42990520bdb9ce5d
whoiz082 <muhammadzeeshan.csec@gmail.com>
2026-06-20 02:59:14 +0500

    Integrate packet parsing into analyze_packet function
    Refactor packet analysis to use parse_packet function.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from core.capture import start_capture
+from core.parser import parse_packet
+
 
 
 def analyze_packet(packet):
 
-    print(packet.summary())
+    data = parse_packet(packet)
+
+    if data:
+
+        print("\n--- Packet Information ---")
+
+        for key, value in data.items():
+
+            print(f"{key}: {value}")
+
 
 
 if __name__ == "__main__":
